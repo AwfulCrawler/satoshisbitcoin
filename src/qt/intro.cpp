69512cd7d195001ad99488c114d00f3c42343f7c
satoshisbitcoin <satoshisbitcoin@outlook.com>
2016-03-13 21:02:03 -0700

    Switched QT messaging to Satoshi's Bitcoin

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -182,7 +182,7 @@ void Intro::pickDataDirectory()
                 TryCreateDirectory(GUIUtil::qstringToBoostPath(dataDir));
                 break;
             } catch (const fs::filesystem_error&) {
-                QMessageBox::critical(0, tr("Bitcoin Classic"),
+                QMessageBox::critical(0, tr("Satoshi's Bitcoin"),
                     tr("Error: Specified data directory \"%1\" cannot be created.").arg(dataDir));
                 /* fall through, back to choosing screen */
             }
